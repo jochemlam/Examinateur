340ca14abe16332bf1c7042b8ff3ddf8497bf6a2
jochemlam <lammertsmajochem@gmail.com>
2022-03-18 15:02:20 +0100

    forgot to add exam class last commit

--- Examinateur/src/examinateur/Exam.java
+++ Examinateur/src/examinateur/Exam.java
@@ -1,5 +1,11 @@
 package examinateur;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Exam {
+    private ArrayList<Exercise> exercises = new ArrayList<>();
+
+    private String examName = "nieuw examen";
 
 }
