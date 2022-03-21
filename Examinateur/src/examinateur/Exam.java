934d34f47d92952fa739969807c23111ccfe23b5
MaikeruB <maikelbazuin@gmail.com>
2022-03-21 14:36:06 +0100

    Exam shuffel

--- Examinateur/src/examinateur/Exam.java
+++ Examinateur/src/examinateur/Exam.java
@@ -2,10 +2,25 @@ package examinateur;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Exam {
-    private ArrayList<Exercise> exercises = new ArrayList<>();
+    private ArrayList<Exercise> exercises;
+    private String examName;
 
-    private String examName = "nieuw examen";
+    public Exam(ArrayList<Exercise> exercises, String examName){ // Maakt een nieuw examen aan en de vragen worden geshuffeld
 
+        Collections.shuffle(exercises);
+
+        this.exercises = exercises;
+        this.examName = examName;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public String getExamName() {
+        return examName;
+    }
 }
