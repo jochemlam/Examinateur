243b39eeeaf2485127621fc7f7bc3db4ff68c7a0
SebasMet <92382477+SebasMet@users.noreply.github.com>
2022-03-25 13:39:00 +0100

    Check duplicate

--- Examinateur/src/examinateur/ApplicationManager.java
+++ Examinateur/src/examinateur/ApplicationManager.java
@@ -20,8 +20,6 @@ public class ApplicationManager {
         Menu menu1 = new Menu();
 
         addStudent(new Student(studentName, studentNumber));
-        addStudent(new Student("Pietje", 3434));
-        addStudent(new Student("Jan", 34343));
 
         menu1.ShowList();
         menu1.SelectOption();
@@ -31,12 +29,29 @@ public class ApplicationManager {
     }
 
     public static void addStudent(Student student) {
+        if (ApplicationManager.students.size() == 0)
             students.add(student);
+        else {
+            for (int i = 0; i < ApplicationManager.students.size(); i++) {
+                if (student.getNumber() == ApplicationManager.students.get(i).getNumber()) {
+                    System.out.println("Deze studentnummber is al toegewezen aan een student");
+                } else {
+                    students.add(student);
+                }
+                break;
+            }
+        }
     }
 
-    public static void showStudents(ArrayList<Student> list) {
+
+        public static void showStudents(ArrayList<Student> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i).getName() + " - " + list.get(i).getNumber());
         }
+
+            Menu menu1 = new Menu();
+            menu1.ShowList();
+            menu1.SelectOption();
+
     }
 }
