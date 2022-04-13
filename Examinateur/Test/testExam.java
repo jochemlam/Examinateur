365443ca72992713926f7e83b2e6ca6c24da6049
MaikeruB <maikelbazuin@gmail.com>
2022-04-13 19:03:55 +0200

    testHasStudentPassed gemaakt
    setAmountCorrect is aangemaakt voor deze test

--- Examinateur/Test/testExam.java
+++ Examinateur/Test/testExam.java
@@ -1,15 +1,31 @@
+import examinateur.Answer;
+import examinateur.Exam;
+import examinateur.Exercise;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
-class testExam {
+public class testExam {
 
     @Test
     public void testHasStudentPassed() {
         //Arrange
+        ArrayList<Exercise> questions = new ArrayList<>();
+        questions.add(new Exercise("Over welke poort worden https-websites geladen?", new Answer("443")));
+        questions.add(new Exercise("Welke is GEEN naam van malware?", new Answer("Achilles")));
+        questions.add(new Exercise("Hoe heet het programma waarmee de CIA smart-tv’s afluisterde?", new Answer("Ransomware")));
+        questions.add(new Exercise("Welke computer worm verspreidde zich het snelst?", new Answer("MyDoom")));
+        questions.add(new Exercise("welke vraag is dit?", new Answer("5")));
 
+        Exam exam = new Exam(questions, "Cyber-Security");
         //Act
 
+        exam.setAmountCorrect((short) 3);
+
         //Assert
+        assertEquals(true, exam.hasStudentPassed());
 
     }
 }
