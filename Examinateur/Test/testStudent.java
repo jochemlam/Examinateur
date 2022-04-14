9969ab6fc9f030066f118a4285ed1404de879f61
MaikeruB <maikelbazuin@gmail.com>
2022-04-14 21:48:02 +0200

    test omgedraaid

--- Examinateur/Test/testStudent.java
+++ Examinateur/Test/testStudent.java
@@ -6,6 +6,19 @@ import static org.junit.Assert.assertEquals;
 public class testStudent {
 
     @Test
+    public void testDeleteStudent() {
+        //Arrange
+        int expectedResult = 0;
+        Student testStudent = new Student("Sebastian M", 123);
+        ApplicationManager.students.add(testStudent);
+        //Act
+        ApplicationManager.students.remove(testStudent);
+        int actualResult = ApplicationManager.students.size();
+        //Assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
     public void testAddExamsPassed(){ // Test of AddExamsPassed bij de meegegeven student, 1 toevoegt bijExamsPassed.
         //Arrange
         int expectedResult = 1;
@@ -18,16 +31,4 @@ public class testStudent {
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test
-    public void testDeleteStudent() {
-        //Arrange
-        int expectedResult = ApplicationManager.students.size();
-        Student testStudent = new Student("Sebastian M", 123);
-        ApplicationManager.students.add(testStudent);
-        //Act
-        ApplicationManager.students.remove(testStudent);
-        int actualResult = ApplicationManager.students.size();
-        //Assert
-        assertEquals(expectedResult, actualResult);
-    }
 }
